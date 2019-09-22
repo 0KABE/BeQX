c0a382dad805cd6f9d437614c1e548b05c40fe07
0KABE <RIN.OKAB3@GMAIL.COM>
2019-09-22 22:59:03 +0800

    Remove & Add comments

--- qx/policy.py
+++ qx/policy.py
@@ -21,17 +21,15 @@ def add_server_remote(policy: Dict[str, Dict[str, Union[str, List[str]]]], remot
                    ] = ss_sub_parser.deserialize_urls(ss_sub_urls)
     ssr_nodes: Dict[str, List[SSRNode]
                     ] = ssr_sub_parser.deserialize_urls(ssr_sub_urls)
-
+    # add policies to policy group
     for subscription in remote:
         sub_type: str = subscription["type"].lower()
         url: str = subscription["url"]
         if sub_type == "ss":
-            # sub = ss.RemoteSubscription(subscription["url"])
             for p in subscription["policies"]:
                 policy[p]["policies"].extend(
                     [node.tag for node in ss_nodes[url]])
         elif sub_type == "ssr":
-            # sub = ssr.RemoteSubscription(subscription["url"])
             for p in subscription["policies"]:
                 policy[p]["policies"].extend(
                     [node.remarks for node in ssr_nodes[url]])
