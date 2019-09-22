0d2310da630b057d1d24e5da0d94798812ec68d3
0KABE <RIN.OKAB3@GMAIL.COM>
2019-09-22 22:56:15 +0800

    Using submodule sub_parser

--- qx/policy.py
+++ qx/policy.py
@@ -1,15 +1,37 @@
-import conf_parser.ss as ss
-import conf_parser.ssr as ssr
+from typing import Dict, List, Union
+
+import sub_parser.ss_sub_parser as ss_sub_parser
+import sub_parser.ssr_sub_parser as ssr_sub_parser
+from sub_parser.ss_sub_parser import SSNode
+from sub_parser.ssr_sub_parser import SSRNode
 
 
-def add_server_remote(policy: dict, remote: dict):
+def add_server_remote(policy: Dict[str, Dict[str, Union[str, List[str]]]], remote: List[Dict[str, Union[str, List[str]]]]):
+    # download ss_sub_urls & ssr_sub_urls
+    ss_sub_urls: List[str] = []
+    ssr_sub_urls: List[str] = []
     for subscription in remote:
         sub_type: str = subscription["type"].lower()
+        url: str = subscription["url"]
         if sub_type == "ss":
-            sub = ss.RemoteSubscription(subscription["url"])
-            for p in subscription["policies"]:
-                policy[p]["policies"].extend(sub.nodes)
+            ss_sub_urls.append(url)
         elif sub_type == "ssr":
-            sub = ssr.RemoteSubscription(subscription["url"])
+            ssr_sub_urls.append(url)
+    ss_nodes: Dict[str, List[SSNode]
+                   ] = ss_sub_parser.deserialize_urls(ss_sub_urls)
+    ssr_nodes: Dict[str, List[SSRNode]
+                    ] = ssr_sub_parser.deserialize_urls(ssr_sub_urls)
+
+    for subscription in remote:
+        sub_type: str = subscription["type"].lower()
+        url: str = subscription["url"]
+        if sub_type == "ss":
+            # sub = ss.RemoteSubscription(subscription["url"])
             for p in subscription["policies"]:
-                policy[p]["policies"].extend(sub.nodes)
+                policy[p]["policies"].extend(
+                    [node.tag for node in ss_nodes[url]])
+        elif sub_type == "ssr":
+            # sub = ssr.RemoteSubscription(subscription["url"])
+            for p in subscription["policies"]:
+                policy[p]["policies"].extend(
+                    [node.remarks for node in ssr_nodes[url]])
